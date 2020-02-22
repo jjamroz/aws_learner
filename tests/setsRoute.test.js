d0e22d875cb51c1729a0937d5babfbf330bd4332
Jamroz <jakub.jamroz@capgemini.com>
2020-02-22 23:42:45 +0100

    minor refactor

--- tests/setsRoute.test.js
+++ tests/setsRoute.test.js
@@ -1,19 +1,22 @@
 const app_user_id = 'user';
-let addedSetId;
+let event;
 
-test('should get sets for user', async done => {
-  const lambda = require('../api/sets/getSets');
-  const event = {
-    body: null,
+beforeEach(() => {
+  event = {
     headers: {
       app_user_id: app_user_id
     }
   };
+});
+
+test('should get sets for user', async done => {
+  const lambda = require('../api/sets/getSets');
 
   const res = await lambda.handler(event);
   const items = JSON.parse(res.body);
 
   expect(res.statusCode).toEqual(200);
+  // tbd... waiting for test db
   // expect(items.length).toEqual(9);
   items.forEach(i => {
     expect(i).toHaveProperty('set_id');
@@ -28,14 +31,8 @@ test('should get sets for user', async done => {
 test('should get set by UUID', async done => {
   const lambda = require('../api/sets/getSet');
 
-  const event = {
-    body: null,
-    headers: {
-      app_user_id: app_user_id
-    },
-    pathParameters: {
-      set_id: '7694f382-e10f-43c6-93c4-13369b5e3488'
-    }
+  event.pathParameters = {
+    set_id: '7694f382-e10f-43c6-93c4-13369b5e3488'
   };
 
   const res = await lambda.handler(event);
@@ -53,14 +50,11 @@ test('should get set by UUID', async done => {
 test('should not get set by UUID - wrong user', async done => {
   const lambda = require('../api/sets/getSet');
 
-  const event = {
-    body: null,
-    headers: {
-      app_user_id: 'wrong_user'
-    },
-    pathParameters: {
-      set_id: '7694f382-e10f-43c6-93c4-13369b5e3488'
-    }
+  event.headers = {
+    app_user_id: 'wrong user'
+  };
+  event.pathParameters = {
+    set_id: '7694f382-e10f-43c6-93c4-13369b5e3488'
   };
 
   const res = await lambda.handler(event);
@@ -86,12 +80,7 @@ test('should add new set', async done => {
     ]
   };
 
-  const event = {
-    body: JSON.stringify(set),
-    headers: {
-      app_user_id: app_user_id
-    }
-  };
+  event.body = JSON.stringify(set);
 
   const res = await lambda.handler(event);
   const addedSet = JSON.parse(res.body);
@@ -107,19 +96,8 @@ test('should add new set', async done => {
 
 test('should update set', async done => {
   const lambda = require('../api/sets/updateSet');
-  const getLambda = require('../api/sets/getSets');
 
-  const event = {
-    body: null,
-    headers: {
-      app_user_id: app_user_id
-    }
-  };
-
-  // getting all sets and getting the one added(alternative is to remember its id from previous test... TBD)
-  const { body } = await getLambda.handler(event);
-  const sets = JSON.parse(body);
-  const set = sets.filter(s => s.name === 'added')[0];
+  const set = await getAddedSet('added');
   set.name = 'updated';
 
   event.body = JSON.stringify(set);
@@ -134,22 +112,11 @@ test('should update set', async done => {
 
 test('should delete set', async done => {
   const lambda = require('../api/sets/deleteSet');
-  const getLambda = require('../api/sets/getSets');
 
-  const event = {
-    body: null,
-    headers: {
-      app_user_id: app_user_id
-    }
-  };
-
-  // getting all sets and getting the one added(alternative is to remember its id from previous test... TBD)
-  const { body } = await getLambda.handler(event);
-  const sets = JSON.parse(body);
-  const set_id = sets.filter(s => s.name === 'updated').map(s => s.set_id)[0];
+  const set = await getAddedSet('updated');
 
   event.pathParameters = {
-    set_id: set_id
+    set_id: set.set_id
   };
 
   const res = await lambda.handler(event);
@@ -157,3 +124,12 @@ test('should delete set', async done => {
   expect(res.statusCode).toEqual(200);
   done();
 });
+
+// getting all sets and getting the one added(alternative is to remember its id from previous test... TBD)
+const getAddedSet = async name => {
+  const lambda = require('../api/sets/getSets');
+
+  const { body } = await lambda.handler(event);
+  const sets = JSON.parse(body);
+  return sets.filter(s => s.name === name)[0];
+};
